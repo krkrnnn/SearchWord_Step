c83a8099c85676180f0cf4ad87271baff3c7d053
福田かるな <ring.ring.0114@gmail.com>
2016-05-28 22:31:30 +0900

    辞書の中身をArrayListに保存

--- SearchWord.java
+++ SearchWord.java
@@ -4,35 +4,8 @@ import java.io.*;
 public class SearchWord{
     public static void main(String[] args){
 
-	List<String> array = new ArrayList<String>();
-	array.add("a");
-	array.add("x");
-	array.add("1");
-	array.add("b");
-	array.add("3");
-	Collections.sort(array);
-	for (String string : array) {
-	    System.out.println(string);
-	}
-
-	try {
-	    BufferedReader br = new BufferedReader(new FileReader("/usr/share/dict/words"));
-	    try {
-		while (true) {
-		    String line = br.readLine();
-            if (line == null) {
-                break;
-            }
-            Collections.sort(line);
-	    System.out.println(line.toUpperCase());
-		}
-	    } finally {
-		br.close();
-	    }
-	} catch (IOException e) {
-	    e.printStackTrace();
-	}
-	
+	//辞書の単語を保存するArrayList
+	ArrayList<String> array = new ArrayList<String>();
 	Scanner sc = new Scanner(System.in);
 	System.out.println("16文字のアルファベットを入力してください");
 	//入力された文字を配列に追加する
@@ -51,15 +24,39 @@ public class SearchWord{
 	}
 	System.out.println(sa);
 
+	try {
+	    BufferedReader br = new BufferedReader(new FileReader("/usr/share/dict/words"));
+	    try {
+	    while (true) {
+		    String line = br.readLine();
+		    //listに辞書の単語を追加
+		    array.add(line);
+            if (line == null) {
+                break;
+            }
+	    //System.out.println(line.toUpperCase());
+		}
+	    } finally {
+		br.close();
+	    }
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
 
+	//辞書を格納したリストの中身の確認
+	for(int i = 0; i < array.size()-1; i++){
+	    String country = array.get(i);
+	    System.out.println(country);
+	}
 	
+
     }
 
     //与えられた配列(文字列)を辞書式にソートするクラス
-    public static void sort(String[] a){
+ public static void sort(String[] a){
 	String tmp = "";
-	for(int i = 0; i < 15; i++){
-	    for(int j = 15; j > i; j--){
+	for(int i = 0; i < a.length - 1; i++){
+	    for(int j = a.length - 1; j > i; j--){
 		if(a[j-1].compareTo(a[j]) > 0){
 		    tmp = a[j];
 		    a[j] = a[j - 1];
